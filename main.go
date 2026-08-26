d7703b8d06769baa9aa953ad74a506f409b39e6a
Arjuna <arjunaragilputera@gmail.com>
2026-08-26 11:24:49 +0700

    fix: foce shutdown doesnt trigger graceful windows return

--- main.go
+++ main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
+	"os"
+	"os/signal"
+	"syscall"
+
 	"github.com/getlantern/systray"
 )
 
 func main() {
+	go func() {
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+		<-c
+		systray.Quit()
+	}()
+
 	systray.Run(onReady, onExit)
 }
